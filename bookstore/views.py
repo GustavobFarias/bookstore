615892056c2ab8d5c238c96ce706a6631b1af579
Gustavo <gustavoburgerf@gmail.com>
2025-07-29 17:45:51 -0300

    ajustando a view

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,27 +1,20 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-
 import git
-
 
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo('/home/drsantos20/bookstore')
-        origin = repo.remotes.origin
-
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
-    else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
-
+        try:
+            repo = git.Repo('/home/drsantos20/bookstore')
+            origin = repo.remotes.origin
+            origin.pull()
+            return HttpResponse("✅ Código atualizado com sucesso!", status=200)
+        except Exception as e:
+            return HttpResponse(f"❌ Erro ao atualizar o código: {str(e)}", status=500)
+    return HttpResponse("Método não permitido", status=405)
 
 def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('hello_world.html')
+    return HttpResponse(template.render({}, request))
