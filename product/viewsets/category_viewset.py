975bd744066e0885cdd02c59117cc0bd87e7fdcb
Gustavo <gustavoburgerf@gmail.com>
2025-07-28 21:06:36 -0300

    Fazendo a authentication no order e product

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,10 +1,13 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Category
 from product.serializers.category_serializer import CategorySerializer
 
-
 class CategoryViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = CategorySerializer
 
     def get_queryset(self):
